30f2241bdef8059b8f0c8e77ff70fd12ed03cf64
positivegrid167 <109517967+positivegrid167@users.noreply.github.com>
2022-07-21 01:48:01 +0530

    07_code updated

--- 07_Typecasting.py
+++ 07_Typecasting.py
@@ -1,4 +1,4 @@
 a = "0712"
 a = int(a) # this line trying to convert "0712" a string to an integer
-print(type(a)) # this line to check type
+print(type(a)) # this line to check type is
 print(a + 5)
