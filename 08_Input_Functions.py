b73f24f538bfef0b97f4394ccc58ea26c62b3190
positivegrid167 <109517967+positivegrid167@users.noreply.github.com>
2022-07-21 18:49:10 +0530

    upgrading

---
+++ 08_Input_Functions.py
@@ -0,0 +1,3 @@
+a = input("Enter your name: ")  # This type remain 'str' until u change type
+a = int(a)                      # Converting it into a 'int' or any type
+print(a)                        
