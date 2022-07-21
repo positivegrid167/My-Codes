64ae808eec14e537cdb5f0441cbf9782057933e4
positivegrid167 <109517967+positivegrid167@users.noreply.github.com>
2022-07-21 19:02:42 +0530

    update now

---
+++ 001_Practice_avg_cal.py
@@ -0,0 +1,6 @@
+a = input("Input first number: ")   #srt
+b = input("Input second number: ")  #str
+a = int(a)                          #converting types
+b = int(b)                          #converting types
+avg = (a + b)/2                     #avg formula
+print("The average of a & b is: ", avg)
